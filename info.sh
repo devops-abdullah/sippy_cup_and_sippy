a7ad6de7e0e5d7e8ff201d51094e0ffd954c980f
devops-abdullah <66333235+devops-abdullah@users.noreply.github.com>
2022-12-22 13:02:38 +0500

    Create info.sh

---
+++ info.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+
+echo '
+##################################################################################
+#                                                                                #
+#   Author: Abdullah Manzoor                                                     #
+#   Email: abdullah.itdeft@gmail.com                                             #
+#   Youtube: https://www.youtube.com/channel/UCwqTU1vhes8u5yqXyhW3xoA            #
+#   Facebook: https://www.facebook.com/Dev0ps                                    #
+#   Twitter: https://twitter.com/devOps_axprt                                    #
+#   Instagram: https://www.instagram.com/manzoor5152/                            #
+#   UpWork: https://www.upwork.com/freelancers/~01affa866a06d807df?viewMode=1    #
+#                                                                                #
+##################################################################################'
+
+echo '
+###################################### Information ######################################
+# Please check Git for Further Guide: 
+#     -   SIPPY_CUP: https://github.com/mojolingo/sippy_cup
+#     -   SIPPY: https://github.com/SIPp/sipp/releases/download/v3.6.1/sipp-3.6.1.tar.gz
+
+################################ How to RUN Information ##################################
+# 1. Install the Docker on your mahcine
+# 2. Start the Docker daemon on system
+# 3. Run the following command to pull the SIPPY_CUP docker.
+#     -   docker pull devopsengineerabdullah/sippy_cup:v3.6.1
+# 4. Run the following command to run the docker.
+#     -   docker run -it --rm --network host --name sippy_cup devopsengineerabdullah/sippy_cup:v3.6.1 bash
+# 5. Enjoy the SIPPY-CUP with SIPPY build package.'
+
+exit 0
